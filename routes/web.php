0ee9f49a5eda4160ded3e72392b2665a25aab75e
Sizan <mahatosizan@gmail.com>
2025-02-25 11:56:09 +0545

    “pages”

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\BlogDetail;
 use App\Livewire\ShowBlog;
 use App\Livewire\ShowFaqPage;
 use App\Livewire\ShowHome;
+use App\Livewire\ShowPage;
 use App\Livewire\ShowService;
 use App\Livewire\ShowServicePage;
 use App\Livewire\ShowTeamPage;
@@ -20,6 +21,7 @@ Route::get('/team',ShowTeamPage::class)->name('team');
 Route::get('/blog',ShowBlog::class)->name('blog');
 Route::get('/blog/{id}',BlogDetail::class)->name('blogDetail');
 Route::get('/faq',ShowFaqPage::class)->name('faq');
+Route::get('/page/{id}',ShowPage::class)->name('page');
 
 
 
